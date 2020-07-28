05628cf7f6d5d08a01dfc336f623ffbbb2a3a4e8
Sho0224 <shootake0224@gmail.com>
2020-07-29 01:36:10 +0900

    Classify

--- BitEditor.py
+++ BitEditor.py
@@ -4,24 +4,29 @@ import logging
 def main():
     pass
 
-def Write(bit_count,value,target,index,empty_bits):
-    logging.debug('value:{}'.format(bin(value)))
-    for i in reversed(range(bit_count)):
-        logging.debug('i:{}'.format(i))
-        logging.debug('index:{}'.format(index))
-        logging.debug('empty_bits:{}'.format(empty_bits))
-        mask = 1 << i
-        one_bit = (value & mask) >> i
-        target[index] += one_bit << (empty_bits - 1) 
-        empty_bits-=1
-        logging.debug('one_bit:{}'.format(bin(one_bit)))
-        logging.debug('target[{}]:{}'.format(index,bin(target[index])))
+class BitEditor:
+    def __init__(self):
+        self.index = 0
+        self.empty_bits = 8
 
-        if empty_bits == 0:
-            index+=1
-            empty_bits = 8
-            target[index] = 0
+    def Write(self,bit_count,value,target):
+        logging.debug('value:{}'.format(bin(value)))
+        for i in reversed(range(bit_count)):
+            logging.debug('i:{}'.format(i))
+            logging.debug('self.index:{}'.format(self.index))
+            logging.debug('self.empty_bits:{}'.format(self.empty_bits))
+            mask = 1 << i
+            one_bit = (value & mask) >> i
+            target[self.index] += one_bit << (self.empty_bits - 1) 
+            self.empty_bits-=1
+            logging.debug('one_bit:{}'.format(bin(one_bit)))
+            logging.debug('target[{}]:{}'.format(self.index,bin(target[self.index])))
+
+            if self.empty_bits == 0:
+                self.index+=1
+                self.empty_bits = 8
+                target[self.index] = 0
     
-    return target,index,empty_bits
+        return target
 
 if __name__ == '__main__': main()
--- test_BitEditor.py
+++ test_BitEditor.py
@@ -1,5 +1,5 @@
 import unittest
-import BitEditor as be
+import BitEditor as be 
 
 class BitEditorTest(unittest.TestCase):
     def setUp(self):
@@ -8,26 +8,25 @@ class BitEditorTest(unittest.TestCase):
         pass
     def test_normal(self):
         bytes = bytearray(range(4))
-        index = 0
-        empty_bits = 8
+        bitEditor = be.BitEditor()
 
-        bytes,index,empty_bits = be.Write(1,0b1,bytes,index,empty_bits)
+        bytes = bitEditor.Write(1,0b1,bytes)
         self.assertEqual(0b10000000,bytes[0])
-        bytes,index,empty_bits = be.Write(2,0b10,bytes,index,empty_bits)
+        bytes = bitEditor.Write(2,0b10,bytes)
         self.assertEqual(0b11000000,bytes[0])
-        bytes,index,empty_bits = be.Write(3,0b101,bytes,index,empty_bits)
+        bytes = bitEditor.Write(3,0b101,bytes)
         self.assertEqual(0b11010100,bytes[0])
-        bytes,index,empty_bits = be.Write(4,0b1001,bytes,index,empty_bits)
+        bytes = bitEditor.Write(4,0b1001,bytes)
         self.assertEqual(0b11010110,bytes[0])
         self.assertEqual(0b01000000,bytes[1])
-        bytes,index,empty_bits = be.Write(5,0b10001,bytes,index,empty_bits)
+        bytes = bitEditor.Write(5,0b10001,bytes)
         self.assertEqual(0b11010110,bytes[0])
         self.assertEqual(0b01100010,bytes[1])
-        bytes,index,empty_bits = be.Write(6,0b100001,bytes,index,empty_bits)
+        bytes = bitEditor.Write(6,0b100001,bytes)
         self.assertEqual(0b11010110,bytes[0])
         self.assertEqual(0b01100011,bytes[1])
         self.assertEqual(0b00001000,bytes[2])
-        bytes,index,empty_bits = be.Write(7,0b1000001,bytes,index,empty_bits)
+        bytes = bitEditor.Write(7,0b1000001,bytes)
         self.assertEqual(0b11010110,bytes[0])
         self.assertEqual(0b01100011,bytes[1])
         self.assertEqual(0b00001100,bytes[2])
